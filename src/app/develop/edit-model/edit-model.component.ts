3fbf7fe19b8fb829996aa290f26126494836ec17
Mathieu Maes <mathieu@zimmo.be>
2017-06-06 22:02:15 +0200

    Add travis

--- src/app/develop/edit-model/edit-model.component.ts
+++ src/app/develop/edit-model/edit-model.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {ModelsService} from '../../shared/domain/models.service';
 
 @Component({
-  selector: 'app-edit-model',
-  templateUrl: './edit-model.component.html',
-  styleUrls: ['./edit-model.component.css']
+    selector: 'app-edit-model',
+    templateUrl: './edit-model.component.html',
+    styleUrls: ['./edit-model.component.css']
 })
-export class EditModelComponent implements OnInit {
+export class EditModelComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+    $model;
+    isInitialized = false;
+    modelSub;
+    constructor(
+        models: ModelsService,
+    ) {
+        this.$model = models.getActiveModel();
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+
+        this.modelSub = this.$model.subscribe((model) => {
+            if (!model) {
+                this.isInitialized = false;
+                return;
+            }
+            this.isInitialized = true;
+        });
+    }
+
+    ngOnDestroy() {
+        this.modelSub.unsubscribe();
+    }
 
 }
