72e6adafde77ff85fb8002a8600a4fbebda6bb4e
Mathieu Maes <mathieu@webberig.be>
2017-08-19 15:56:45 +0000

    Create a PublishSubject for triggering debounced retrieval of data

--- src/app/shared/domain/sites.service.ts
+++ src/app/shared/domain/sites.service.ts
@@ -6,66 +6,50 @@ import {request, reject, resolve} from 'redux-promised';
 import {GET_SITES, GET_SITE, SET_ACTIVE_SITE, UPDATE_SITE, CREATE_SITE, DELETE_SITE} from '../../store/actionTypes';
 import {ISitesState} from '../../store/sites/index';
 import {ApiService} from '../api.service';
-import {Observable} from 'rxjs/Observable';
+import {Observable, PublishSubject} from 'rxjs/Observable';
 import 'rxjs/add/observable/combineLatest';
 import {ISitesListState} from '../../store/sites/sitesList';
 import {getItemById} from '../arrayUtils';
 
 @Injectable()
 export class SitesService {
+    getAvailableSitesSubject = new PublishSubject();
+    
     constructor(
         private ngRedux: NgRedux<any>,
         private api: ApiService,
     ) {
+        this.getAvailableSitesSubject
+        .debounce(() => Observable.timer(5000))
+        .subscribe(() => {
+            this.ngRedux.dispatch({
+                type: request(GET_SITES)
+            });
+            this.api.get('/sites')
+                .map(result => {
+                    return result.json();
+                })
+                .subscribe(
+                    result => this.ngRedux.dispatch({
+                        type: resolve(GET_SITES),
+                        payload: result
+                    }),
+                    err => this.ngRedux.dispatch({
+                        type: reject(GET_SITES),
+                        error: err,
+                    })
+                );
 
+        });
     }
 
     getAvailableSites() {
-        this.ngRedux.dispatch({
-            type: request(GET_SITES)
-        });
-        this.api.get('/sites')
-            .map(result => {
-                return result.json();
-            })
-            .subscribe(
-                result => this.ngRedux.dispatch({
-                    type: resolve(GET_SITES),
-                    payload: result
-                }),
-                err => this.ngRedux.dispatch({
-                    type: reject(GET_SITES),
-                    error: err,
-                })
-            );
+        this.getAvailableSitesSubject.next();
         return this.getSitesListStream();
     }
 
     getSite(id) {
-        this.ngRedux.dispatch({
-            type: request(GET_SITE),
-            meta: {
-                id,
-            },
-        });
-        this.api.get(`/sites/${id}`)
-            .map(result => {
-                return result.json();
-            })
-            .subscribe(
-                result => this.ngRedux.dispatch({
-                    type: resolve(GET_SITE),
-                    meta: { id },
-                    payload: result
-                }),
-                err => this.ngRedux.dispatch({
-                    type: reject(GET_SITE),
-                    meta: { id },
-                    error: err,
-                })
-            );
-        return this.ngRedux
-            .select('sites')
+        return this.getAvailableSites()
             .map((state: ISitesState) => state.sitesList.items)
             .map(items => {
                 for (let site of items) {
